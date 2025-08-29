a7bc5ca2e25239a37c266fba7dfa874bb2123e82
shatakratu <vishoka@proton.me>
2025-08-29 22:13:27 +0700

    Update variables.tf / Terraform cloud test

--- network/variables.tf
+++ network/variables.tf
@@ -3,3 +3,9 @@
 #   default     = "xxxxxxxxxxxxxxxxxx"
 #   description = "sattvika folder id"
 # }
+
+variable "sattvika_folder" {
+  description = "Folder ID in Yandex Cloud"
+  type        = string
+  sensitive   = true
+}
